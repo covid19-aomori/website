4ecf9543061862160e1a2d43b2762ea658ded4b4
youyo <1003ni2@gmail.com>
2020-03-31 08:42:41 +0900

    netlify build test

--- plugins/netlify-plugin-fetch-data-source/index.js
+++ plugins/netlify-plugin-fetch-data-source/index.js
@@ -1,5 +1,11 @@
 module.exports = {
   onPreBuild: () => {
-    console.log('Hello world from onPreBuild event!')
+    const rp = require('request-promise')
+    const fs = require('fs')
+
+    const url =
+      'https://covid19-aomori-production-dataset-store.s3-ap-northeast-1.amazonaws.com/data.json'
+    const file = fs.createWriteStream('./data/data.json')
+    rp(url).pipe(file)
   }
 }
